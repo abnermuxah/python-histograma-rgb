88ff002f543133496a638f212e046c33aa226fb7
Abner Santana <FineLine>
2020-04-20 19:09:26 -0300

    separar canais r g b e distribuir no histograma

--- histogram-rgb.py
+++ histogram-rgb.py
@@ -7,25 +7,22 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread("lena.png")
-#img = np.zeros((200,200), np.uint8)
-#cv.rectangle(img, (0, 100), (200, 200), (255), -1)
-#cv.rectangle(img, (0, 50), (100, 100), (127), -1)
+img = cv.imread("lena.png") # aceita formatos extras .jpg, png e .tiff
 b, g, r = cv.split(img)
-cv.imshow("img", img)
-#cv.imshow("b", b)
-#cv.imshow("g", g)
-#cv.imshow("r", r)
+cv.imshow("Resultado", img)
 
-plt.hist(b.ravel(), 256, [0, 256])
+red = plt.subplot(331)
+red = plt.hist(r.ravel(), 256, [0, 256], color="red")
 
-plt.hist(g.ravel(), 256, [0, 256])
+green = plt.subplot(332)
+green = plt.hist(g.ravel(), 256, [0, 256], color="green")
 
-plt.hist(r.ravel(), 256, [0, 256])
+blue = plt.subplot(333)
+blue = plt.hist(b.ravel(), 256, [0, 256], color="blue")
 
-#hist = cv.calcHist([img], [0], None, [256], [0, 256])
-#plt.plot(hist)
+rgb = plt.subplot(212)
+rgb = plt.hist(r.ravel(), 256, [0, 256], color="red") + plt.hist(g.ravel(), 256, [0, 256], color="green") + plt.hist(b.ravel(), 256, [0, 256], color="blue" ) 
+
+
 plt.show()
-
-cv.waitKey(0)
 cv.destroyAllWindows()
